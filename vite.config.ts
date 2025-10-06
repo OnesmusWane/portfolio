70ded7da1ebd8149ed4f93577bba9ee8ca0ef312
WayneOnesmus <onesmus@my1health.com>
2025-10-06 17:33:25 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/portfolio/' : '/', // use base only in production (GitHub Pages)
   plugins: [react()],
-   server: {
+  server: {
     host: true, // allows external access
-    allowedHosts: ['fe1c113949e9.ngrok-free.app'] // allow your ngrok host
-  }
-})
+    allowedHosts: ['fe1c113949e9.ngrok-free.app'], // allow your ngrok host
+  },
+}))
